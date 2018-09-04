22626cec255963d8f0d0d2e3b72ac6af3aa2dcc3
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-09-04 22:53:20 +0200

    SASS files cleanup. Added padding to each page.

--- AutoServiceBook/ClientApp/src/components/NavMenu.jsx
+++ AutoServiceBook/ClientApp/src/components/NavMenu.jsx
@@ -2,44 +2,74 @@
 import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import AuthenticationService from '../utils/authentication/AuthenticationService';
 
 export class NavMenu extends Component {
   displayName = NavMenu.name
+
+  constructor() {
+    super();
+    this.auth = new AuthenticationService();
+  }
 
   render() {
     return (
       <Navbar inverse fixedTop fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to={'/'}>AutoServiceBook</Link>
+            <Link to={'/'}><Glyphicon glyph='wrench' /> AutoServiceBook</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='home' /> Home
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/counter'}>
-              <NavItem>
-                <Glyphicon glyph='education' /> Counter
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
-              <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/login'}>
-              <NavItem>
-                <Glyphicon glyph='log-in' /> Login
-              </NavItem>
-            </LinkContainer>
+            <MenuLinks isLoggedIn={this.auth.isUserLoggedIn()} />
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     );
   }
+}
+
+function MenuLinks(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <AuthorizedMenuLinks />;
+  }
+
+  return <NotAuthorizedMenuLinks />
+}
+
+function AuthorizedMenuLinks() {
+  return (
+    <React.Fragment>
+      <LinkContainer to={'/home'}>
+        <NavItem>
+          <Glyphicon glyph='home' /> Home
+      </NavItem>
+      </LinkContainer>
+      <LinkContainer to={'/logout'}>
+        <NavItem>
+          <Glyphicon glyph='log-out' /> Logout
+      </NavItem>
+      </LinkContainer>
+    </React.Fragment>
+  );
+}
+
+function NotAuthorizedMenuLinks() {
+  return (
+    <React.Fragment>
+      <LinkContainer to={'/register'}>
+        <NavItem>
+          <Glyphicon glyph='registration-mark' /> Register
+      </NavItem>
+      </LinkContainer>
+      <LinkContainer to={'/login'}>
+        <NavItem>
+          <Glyphicon glyph='log-in' /> Login
+      </NavItem>
+      </LinkContainer>
+    </React.Fragment>
+  );
 }

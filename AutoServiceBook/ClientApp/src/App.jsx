828703cd7a70f4d8f47ea0e91ce79ce60e45d8a9
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-09-11 23:33:08 +0200

    Added index.js imports files

--- AutoServiceBook/ClientApp/src/App.jsx
+++ AutoServiceBook/ClientApp/src/App.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { HomePage } from './pages/HomePage';
-import { LoginPage } from './pages/LoginPage';
-import { LogoutPage } from './pages/LogoutPage';
-import { AccountPage } from './pages/AccountPage';
+import { Layout } from './components';
+import { HomePage, LoginPage, LogoutPage, AccountPage, RegisterPage } from './pages';
 import { PropsRoute, PublicRoute, PrivateRoute } from 'react-router-with-props';
 import AuthenticationService from './utils/authentication/AuthenticationService';
 
@@ -15,9 +12,10 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
+        <PublicRoute path='/login' authed={Auth.isUserLoggedIn()} redirectTo="/" component={LoginPage} />
+        <PublicRoute path='/register' authed={Auth.isUserLoggedIn()} redirectTo="/" component={RegisterPage} />
         <PrivateRoute exact path="/" authed={false} redirectTo="/home" component={HomePage} />
         <PrivateRoute path="/home" authed={Auth.isUserLoggedIn()} redirectTo="/login" component={HomePage} />
-        <PublicRoute path='/login' authed={Auth.isUserLoggedIn()} redirectTo="/" component={LoginPage} />
         <PrivateRoute path="/logout" authed={Auth.isUserLoggedIn()} redirectTo="/login" component={LogoutPage} />
         <PrivateRoute path="/account" authed={Auth.isUserLoggedIn()} redirectTo="/login" component={AccountPage} />
       </Layout>

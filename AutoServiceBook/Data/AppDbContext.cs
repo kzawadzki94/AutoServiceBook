f883a639d8ad9be2b4f4421d050bd888de642620
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-10-15 22:03:36 +0200

    Added DbSet for Vehicles along with migration

--- AutoServiceBook/Data/AppDbContext.cs
+++ AutoServiceBook/Data/AppDbContext.cs
@@ -8,5 +8,7 @@ namespace AutoServiceBook.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
     }
 }
---
+++ AutoServiceBook/Migrations/20181015200033_Vehicles.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoServiceBook.Migrations
+{
+    public partial class Vehicles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OwnerId = table.Column<string>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    LicencePlate = table.Column<string>(nullable: false),
+                    Make = table.Column<string>(nullable: false),
+                    Model = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Vin = table.Column<string>(nullable: true),
+                    EngineDisplacement = table.Column<int>(nullable: false),
+                    Mileage = table.Column<long>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false),
+                    InsuranceExpireDate = table.Column<DateTime>(nullable: false),
+                    NextServiceDate = table.Column<DateTime>(nullable: false),
+                    InsuranceNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.CarId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}

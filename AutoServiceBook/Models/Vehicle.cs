635e686927233c5f127412986b7c0ad35a02dbe6
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-10-15 21:50:10 +0200

    Implemented vehicle model

---
+++ AutoServiceBook/Models/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoServiceBook.Models
+{
+    public class Vehicle
+    {
+        [Required]
+        public string OwnerId { get; set; }
+
+        [Required]
+        public VehicleType Type { get; set; }
+
+        [Required]
+        public string LicencePlate { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        public string Vin { get; set; }
+
+        public int EngineDisplacement { get; set; }
+
+        public long Mileage { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RegisterDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime InsuranceExpireDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime NextServiceDate { get; set; }
+
+        public string InsuranceNumber { get; set; }
+    }
+}

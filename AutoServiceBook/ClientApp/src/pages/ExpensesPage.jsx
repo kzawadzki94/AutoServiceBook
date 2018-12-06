e905d9372d6f9a88fe28471590c768c29e335dfd
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-12-06 21:47:23 +0100

    Implemented expenses fetching.

--- AutoServiceBook/ClientApp/src/pages/ExpensesPage.jsx
+++ AutoServiceBook/ClientApp/src/pages/ExpensesPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, ButtonGroup, ButtonToolbar, Glyphicon, Well, PanelGroup, Panel } from 'react-bootstrap';
 import AuthenticationService from '../utils/authentication/AuthenticationService';
+import ExpensesService from '../utils/expenses/ExpensesService';
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import toastr from 'toastr';
@@ -11,17 +12,32 @@ export class ExpensesPage extends Component {
         super();
 
         this.auth = new AuthenticationService();
+        this.expensesService = new ExpensesService(this.auth);
 
         this.state = {
+            expenses: null,
+            isLoading: true
         };
     }
 
     componentDidMount() {
+        this.fetchExpenses();
     }
 
     componentDidUpdate(prevProps, prevState) {
-        //if (prevState.isLoading !== this.state.isLoading) {
-        //}
+        if (prevState.isLoading !== this.state.isLoading) {
+            this.fetchExpenses();
+        }
+    }
+
+    fetchExpenses = () => {
+        this.expensesService.getExpenses()
+            .then(response => {
+                this.setState({
+                    expenses: response,
+                    isLoading: false
+                });
+            });
     }
 
     render() {
